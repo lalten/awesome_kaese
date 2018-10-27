bf35ce8c7c50cb2d77d122e4db29acca02c94350
Nikolas Engelhard <engelhard@magazino.eu>
2018-10-28 00:55:26 +0200

    lint, more features

--- scripts/clean_zone.py
+++ scripts/clean_zone.py
@@ -3,18 +3,21 @@
 import rospy
 import cv2
 import numpy as np
-from tf2_msgs.msg import LookupTransformAction, LookupTransformGoal, LookupTransformResult
+from tf2_msgs.msg import LookupTransformAction, LookupTransformGoal, LookupTransformResult, TFMessage
 from actionlib import SimpleActionClient
-from nav_msgs.msg import OccupancyGrid
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
+# This class
 class CleanZone:
 
     def __init__(self):
         self.map_img = None
+        self.cpy_img = None
 
         hector_ns = "/hector_mapping/"
         self.resolution = rospy.get_param(hector_ns+"map_resolution")
-        self.cells_per_axis = rospy.get_param(hector_ns+"map_size")
+        self.cells_per_axis = int(rospy.get_param(hector_ns+"map_size")*0.55)  # just for video
 
         self.border_length = self.resolution*self.cells_per_axis
         rospy.loginfo("Using map size of %.2f m and resolution of %.2f m",
@@ -23,25 +26,26 @@ class CleanZone:
         self.map_frame = rospy.get_param(hector_ns + "map_frame")
         self.base_frame = rospy.get_param(hector_ns + "base_frame")
 
-        rospy.loginfo("Map frame: '%s', robot base frame: '%s'", self.map_frame, self.base_frame)
+        self.pub_img = rospy.Publisher("clean_img", Image, queue_size=1)
 
-        self.tf_client = SimpleActionClient("/tf2_buffer_server", LookupTransformAction)
-        if not self.tf_client.wait_for_server(rospy.Duration(2)):
-            rospy.logerr("no tf2 available!")
+        rospy.loginfo("Map frame: '%s', robot base frame: '%s'", self.map_frame, self.base_frame)
 
         rospy.loginfo("starting cleaning map")
         self.start_clean_zone_map()
+        self.bridge = CvBridge()
 
-        self.update_timer = rospy.Timer(rospy.Duration(1), self.timer_cb)
+        self.use_tf2_action = False
 
-        self.pub_clean_map = rospy.Publisher("clean_map", OccupancyGrid)
+        if self.use_tf2_action:
+            self.tf_client = SimpleActionClient("/tf2_buffer_server", LookupTransformAction)
+            if not self.tf_client.wait_for_server(rospy.Duration(2)):
+                rospy.logerr("no tf2 available!")
 
-        og = OccupancyGrid()
-
-
-        # self._metric_to_pixel(0)
-        # self._metric_to_pixel(25)
-        # self._metric_to_pixel(-25)
+            self.update_timer = rospy.Timer(rospy.Duration(1), self.timer_cb)
+        else:
+            self.pub_tf = rospy.Subscriber("/tf", TFMessage, self.tf_cb, queue_size=1)
+            self.last_tf_time = 0
+            self.diff_tf_time = rospy.Duration(0.5)
 
     def _get_position(self):
         goal = LookupTransformGoal()
@@ -53,7 +57,6 @@ class CleanZone:
             return None
 
         res = self.tf_client.get_result()
-        # print res
 
         assert isinstance(res, LookupTransformResult)
         if res.error.error != 0:
@@ -62,17 +65,41 @@ class CleanZone:
 
         return res.transform.transform.translation
 
-    def timer_cb(self, timer_event):
-        rospy.loginfo("Timer cb")
-        pos = self._get_position()
-        px_x = self._metric_to_pixel(pos.x)
-        px_y = self._metric_to_pixel(pos.y)
+    def tf_cb(self, tfm):
+        assert isinstance(tfm, TFMessage)
+        for t in tfm.transforms:
+            if t.header.frame_id == self.map_frame and t.child_frame_id == self.base_frame:
+                if self.last_tf_time == 0 or self.last_tf_time + self.diff_tf_time < t.header.stamp:
+                    self.draw_at_position(t.transform.translation)
+                    self.last_tf_time = t.header.stamp
+
+    def draw_at_position(self, position):
+        px_x = self._metric_to_pixel(position.x)
+        px_y = self._metric_to_pixel(position.y)
 
         if not (px_x and px_y):
             return
 
-        cv2.circle(self.map_img, (px_x, px_y), 30, 255, -1)
+        px_y = self.cells_per_axis - px_y
+
+        radius = 20
+        new_col = 30
+
+        cv2.circle(self.cpy_img, (px_x, px_y), radius, new_col, -1)
+        cv2.add(self.map_img, self.cpy_img, self.map_img)
+
+        cv2.circle(self.cpy_img, (px_x, px_y), radius, 0, -1)
+
+        cv_image = self.bridge.cv2_to_imgmsg(self.map_img, encoding="mono8")
+        self.pub_img.publish(cv_image)
+
         cv2.imwrite("/tmp/map.png", self.map_img)
+
+    def timer_cb(self, _):
+        pos = self._get_position()
+        if not pos:
+            return
+        self.draw_at_position(pos)
 
     def _metric_to_pixel(self, d):
         # assuming map_start_x/y == 0.5 (?)
@@ -88,12 +115,7 @@ class CleanZone:
     def start_clean_zone_map(self):
         rospy.loginfo("Creating new clean map with length %i", self.cells_per_axis)
         self.map_img = np.zeros((self.cells_per_axis, self.cells_per_axis, 1), np.uint8)
-        # cv2.imwrite("/tmp/map.png", self.map_img)
-
-        pos = self._get_position()
-        if not pos:
-            return
-
+        self.cpy_img = np.zeros((self.cells_per_axis, self.cells_per_axis, 1), np.uint8)
 
 if __name__ == "__main__":
     rospy.init_node("clean_zone")
